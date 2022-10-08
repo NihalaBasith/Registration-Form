c1b45420c20df8d735e0154abd45831f883d5999
NihalaBasith <nihalabasith@icloud.com>
2022-10-08 12:20:34 +0530

    first commit

---
+++ main/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+from django.contrib.auth import views as auth_views
+
+from main import views 
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+]
+
